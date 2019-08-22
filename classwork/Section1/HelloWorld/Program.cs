ac92af53ae341027ed796ffee553601028b710cb
cameron.king629 <cameron.king629@NEITD-211215>
2019-08-21 19:02:44 -0500

    Initial version

---
+++ classwork/Section1/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * ISE 1430
+ * Lab 1
+ * Cameron King
+ */
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            Console.WriteLine ("Hello World!");
+        }
+
+    }
+}
