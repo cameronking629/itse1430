abc5fccf7a25a49ac88ef8c9e3063a592a9ba6e1
cameron.king629 <cameron.king629@NEITD-211210>
2019-09-11 19:11:48 -0500

    Created a WinForm and began expanding on architecture

---
+++ classwork/MovieLibrary/Itse1430.MovieLib.Host/MainForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Itse1430.MovieLib.Host
+{
+    public partial class MainForm : Form
+    {
+        public MainForm ()
+        {
+            InitializeComponent ();
+
+            Movie movie = new Movie();
+        }
+    }
+}
